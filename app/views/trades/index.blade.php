876349b05a8913eb7c66183a2f6b993f5d4d55f2
Chris Normansell <chris.normansell@gmail.com>
2014-10-17 09:21:54 +0100

    Started work on displaying all trades

--- app/views/trades/index.blade.php
+++ app/views/trades/index.blade.php
@@ -1,17 +1,30 @@
+   <div class="row row-table-header hidden-xs">
+        <div class='col-sm-3 col-xs-12'>
+			<p>Item Name</p>
+        </div>
 
+        <div class='col-sm-3 col-xs-12'>
+			<p>Buy Price</p>
+        </div>
+
+        <div class='col-sm-3 col-xs-12'>
+			<p>Sell Price</p>
+        </div>
+
+    </div>
 
 @foreach($trades as $trade)
 
-    <div class="row">
-        <div class='col-md-3'>
+    <div class="row trades-row">
+        <div class='col-sm-3 col-xs-12'>
             <p>{{$trade->item_name}}</p>
         </div>
 
-        <div class='col-md-3'>
+        <div class='col-sm-3 col-xs-12'>
             <p>{{$trade->buy_price}}</p>
         </div>
 
-        <div class='col-md-3'>
+        <div class='col-sm-3 col-xs-12'>
             <p>{{$trade->sell_price}}</p>
         </div>
 
