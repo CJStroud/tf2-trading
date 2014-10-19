7592b530396465a70c2d34186247a3cb13b31282
Piefarmer <chris.normansell@gmail.com>
2014-10-19 19:11:41 +0100

    Highlighting finished trades

--- app/views/trades/index.blade.php
+++ app/views/trades/index.blade.php
@@ -24,47 +24,47 @@
 
 @foreach($trades as $trade)
 
-	<div class="row trades-row">
-		<div class='col-sm-2 col-xs-12 row-table-cell'>
-			<p>{{$trade->item_name}}</p>
-		</div>
+<div class="row trades-row @if($trade->sold) bg-success @endif">
+	<div class='col-sm-2 col-xs-12 row-table-cell'>
+		<p>{{$trade->item_name}}</p>
+	</div>
 
-		<div class='col-sm-1 col-xs-12 row-table-cell'>
-			<p>
-				{{ $trade->buy_price }}
-			</p>
-		</div>
+	<div class='col-sm-1 col-xs-12 row-table-cell'>
+		<p>
+			{{ $trade->buy_price }}
+		</p>
+	</div>
 
-		<div class='col-sm-1 col-xs-12 row-table-cell'>
-			<p>
-				{{ $trade->sell_price }}
-			</p>
-		</div>
+	<div class='col-sm-1 col-xs-12 row-table-cell'>
+		<p>
+			{{ $trade->sell_price }}
+		</p>
+	</div>
 
-		<div class='col-sm-2 col-xs-12 row-table-cell'>
-			<p>{{ date('d/m/Y', $trade->buy_date) }}</p>
-		</div>
+	<div class='col-sm-2 col-xs-12 row-table-cell'>
+		<p>{{ date('d/m/Y', $trade->buy_date) }}</p>
+	</div>
 
-		<div class='col-sm-2 col-xs-12 row-table-cell'>
-			@if ($trade->sold)
-				<p>{{ date('d/m/Y', $trade->sell_date) }}</p>
-			@else
-				<p class="text-warning">Unsold</p>
-			@endif
-		</div>
-
-		<div class='col-md-1 col-sm-2 col-xs-6'>
-			<a class="btn btn-success" href="{{ route('trade.edit', ['trade' => $trade->id] ) }}">
-				<i class="glyphicon glyphicon-pencil"></i> Edit</a>
-		</div>
-		@if (!$trade->sold)
-			{{ Form::open(['action' => ['TradeController@sold', $trade->id], 'method' => 'POST']) }}
-			<div class='col-md-1 col-sm-2 col-xs-6'>
-				<button class="btn btn-info">
-					<i class="glyphicon glyphicon-gbp"></i> Sold
-				</button>
-			</div>
-			{{ Form::close() }}
+	<div class='col-sm-2 col-xs-12 row-table-cell'>
+		@if ($trade->sold)
+			<p>{{ date('d/m/Y', $trade->sell_date) }}</p>
+		@else
+			<p class="text-warning">Unsold</p>
 		@endif
 	</div>
+
+	<div class='col-md-1 col-sm-2 col-xs-6'>
+		<a class="btn btn-success" href="{{ route('trade.edit', ['trade' => $trade->id] ) }}">
+			<i class="glyphicon glyphicon-pencil"></i> Edit</a>
+	</div>
+	@if (!$trade->sold)
+		{{ Form::open(['action' => ['TradeController@sold', $trade->id], 'method' => 'POST']) }}
+		<div class='col-md-1 col-sm-2 col-xs-6'>
+			<button class="btn btn-info">
+				<i class="glyphicon glyphicon-gbp"></i> Sold
+			</button>
+		</div>
+		{{ Form::close() }}
+	@endif
+</div>
 @endforeach
